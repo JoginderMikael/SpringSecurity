a768bff05a9073b992420f76945f2c516da7c7e5
Joginder Mikael <jogindermikael@gmail.com>
2025-11-27 15:58:37 +0300

    Initial commit, Spring security. Token generation

---
+++ src/main/java/git/jogindermikael/SpringSecExa/SpringSecExaApplication.java
@@ -0,0 +1,13 @@
+package git.jogindermikael.SpringSecExa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecExaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecExaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/git/jogindermikael/SpringSecExa/SpringSecExaApplicationTests.java
@@ -0,0 +1,13 @@
+package git.jogindermikael.SpringSecExa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecExaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
