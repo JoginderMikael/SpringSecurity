5b4bef5c858d1e2e68816eaf93be5c15037854bd
Joginder Mikael <jogindermikael@gmail.com>
2025-11-29 17:38:21 +0300

    BCrypt password

--- src/main/java/git/jogindermikael/SpringSecExa/service/UserService.java
+++ src/main/java/git/jogindermikael/SpringSecExa/service/UserService.java
@@ -3,6 +3,7 @@ package git.jogindermikael.SpringSecExa.service;
 import git.jogindermikael.SpringSecExa.model.Users;
 import git.jogindermikael.SpringSecExa.repo.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,7 +11,11 @@ public class UserService {
 
     @Autowired
     private UserRepo repo;
+
+    private BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder(12);
+
     public Users registerUser(Users user) {
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
         return repo.save(user);
     }
 }
